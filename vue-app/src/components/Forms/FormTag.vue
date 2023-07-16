b93fe6f4520ddc3a82edcab8820c9702ad891752
Jonathan Doucette <jon@digitalforgedesign.com>
2023-07-16 19:16:00 -0400

    form validation with bootstrap and components

---
+++ vue-app/src/components/Forms/FormTag.vue
@@ -0,0 +1,35 @@
+<template>
+  <form
+    @submit.prevent="submit"
+    :ref="name"
+    :event="event"
+    autocomplete="off"
+    :action="action"
+    :method="method"
+    class="needs-validation"
+    novalidate
+  >
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "FormTag",
+  props: ["method", "action", "name", "event"],
+  methods: {
+    submit() {
+      let myForm = this.$refs[this.$props.name]
+
+      if (myForm.checkValidity()) {
+        console.log("my event name", this.$props["event"])
+        console.log("name", this.$props.name)
+        this.$emit(this.$props["event"])
+      }
+      myForm.classList.add("was-validated")
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ vue-app/src/components/Login.vue
@@ -0,0 +1,57 @@
+<!-- eslint-disable no-extra-semi -->
+<!-- eslint-disable no-extra-semi -->
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="mt-5">Login</h1>
+        <br />
+        <form-tag @myevent="submitHandler" name="myform" event="myevent">
+          <text-input
+            label="Email"
+            type="email"
+            name="email"
+            required="true"
+            v-model="email"
+          />
+          <text-input
+            label="Password"
+            type="password"
+            name="password"
+            required="true"
+            v-model="password"
+          />
+          <input type="submit" class="btn btn-primary" value="Login" />
+        </form-tag>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TextInput from "../components/Forms/TextInput.vue"
+import FormTag from "../components/Forms/FormTag.vue"
+
+export default {
+  name: "Login",
+  components: {
+    "text-input": TextInput,
+    "form-tag": FormTag,
+  },
+  data() {
+    return {
+      email: "",
+      password: "",
+    }
+  },
+  methods: {
+    submitHandler() {
+      console.log(
+        "submitHandler called - success" + this.email + " " + this.password
+      )
+    },
+  },
+}
+</script>
+
+<style></style>
